f5ec18b815c9788bd3bc92b7f3162073e4bf2502
Carlos Hernandez Santes <dicarlomagnus@gmail.com>
2020-12-10 14:38:42 -0600

    adding Crashlytics successfully and a crash button

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.firebase.crashlytics.FirebaseCrashlytics
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var crashButton : Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        crashButton = findViewById(R.id.crash_button)
+        crashButton.setOnClickListener {
+            FirebaseCrashlytics.getInstance().recordException(Exception("test crash app"))
+            throw RuntimeException("Test Crash")
+        }
     }
+
+
 }
